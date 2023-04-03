c3de15491f2396fa2344591102cd2cff3ecec4ee
TheLxh6 <1550475045@qq.com>
2023-04-04 00:37:34 +0800

    初始化

---
+++ src/.vuepress/sidebar/zh.ts
@@ -0,0 +1,19 @@
+import { sidebar } from "vuepress-theme-hope";
+
+export const zhSidebar = sidebar({
+  "/zh/":"structure",
+});
+
+
+// {
+//       "/foo/": "structure",
+
+//       "/bar/": "structure",
+
+//       // fallback
+//       "/": [
+//         "" /* / */,
+//         "contact" /* /contact.html */,
+//         "about" /* /about.html */,
+//       ],
+//     }
